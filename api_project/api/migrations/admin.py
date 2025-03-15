a6c26e13173e24e0d0e8f82af0bc389ac21bf830
GTA__Civilian <karabomunano@gmail.com>
2025-03-15 19:58:45 +0200

    Delete api_project directory

--- api_project/api/migrations/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Book # type: ignore
-
-# Register your models here.
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author']
-    search_fields = ['title', 'author']
