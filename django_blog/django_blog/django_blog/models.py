e5f40414f2fef2c76f36c5572639614c2b3960f1
GTA__Civilian <karabomunano@gmail.com>
2025-03-23 18:35:58 +0200

    Delete django_blog directory

--- django_blog/django_blog/django_blog/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import models
-from .models import model_models
-
-class model_models:
-
-    title:modelsCharField(max_length=200)
-    content:models.textfield()
-    published_date: models.DateTmeFeld(auto_now_add=True)
-    author:models.ForeignKey
